56289535a78d8cbc1049bb97dcf8de84fe6ab548
stinkolka <nik.kovacova97@gmail.com>
2026-02-07 19:00:38 +0100

    Second version of calculator

--- src/KalkulackaGUI.java
+++ src/KalkulackaGUI.java
@@ -1,15 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class KalkulackaGUI extends JFrame {
+import constants.Constants;
+
+public class KalkulackaGUI extends JFrame implements ActionListener {
 
     private JTextField textField;
-    private Button[] numberButton;
+    private JPanel buttonPanel;
+    private JButton[] buttons;
 
     public KalkulackaGUI() {
         super("Kalkulačka");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(450, 550);
+        setSize(Constants.APP_SIZE[0], Constants.APP_SIZE[1]);
         setLocationRelativeTo(null);
         setLayout(null);
         setResizable(false);
@@ -24,11 +29,45 @@ public class KalkulackaGUI extends JFrame {
 
     private void addDisplayComponents() {
         textField = new JTextField();
-        textField.setBounds(10, 10, 415, 100);
+        textField.setBounds(Constants.TEXTFIELD_X, Constants.TEXTFIELD_Y, Constants.TEXTFIELD_WIDTH, Constants.TEXTFIELD_HEIGHT);
         textField.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        textField.setFont(new Font("Arial", Font.PLAIN, Constants.FONT_SIZE));
+        textField.setText("0");
+        textField.setHorizontalAlignment(JTextField.RIGHT);
         add(textField);
     }
 
-    private void addButtonComponents() {}
+    private void addButtonComponents() {
+        buttonPanel = new JPanel();
+        buttonPanel.setBounds(Constants.BUTTON_PANEL_X, Constants.BUTTON_PANEL_Y, Constants.BUTTON_PANEL_WIDTH, Constants.BUTTON_PANEL_HEIGHT);
+        buttonPanel.setLayout(new GridLayout(5, 4, 5, 5));
+
+        String[] buttonLabels = {
+                "<-", "AC", "%", "/",
+                "7", "8", "9", "*",
+                "4", "5", "6", "-",
+                "1", "2", "3", "+",
+                "+/-", "0", ".", "="
+        };
+
+        buttons = new JButton[Constants.BUTTON_COUNT];
+        for(int i = 0; i < Constants.BUTTON_COUNT; i++){
+            buttons[i] = new JButton(buttonLabels[i]);
+            buttons[i].setFont(new Font("Arial", Font.PLAIN, Constants.BUTTON_FONT_SIZE));
+            buttons[i].addActionListener (this);
+            buttonPanel.add(buttons[i]);
+        }
+
+        add(buttonPanel);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
 }
+
+
 
---
+++ src/constants/Constants.java
@@ -0,0 +1,21 @@
+package constants;
+
+public class Constants {
+    public static final int[] APP_SIZE = {400, 600};
+
+    //textField constants
+    public static final int TEXTFIELD_X = 10;
+    public static final int TEXTFIELD_Y= 10;
+    public static final int TEXTFIELD_WIDTH = 365;
+    public static final int TEXTFIELD_HEIGHT = 100;
+    public static final int FONT_SIZE = 30;
+
+    //button panel constants
+    public static final int BUTTON_PANEL_X = 10;
+    public static final int BUTTON_PANEL_Y = 130;
+    public static final int BUTTON_PANEL_WIDTH = 365;
+    public static final int BUTTON_PANEL_HEIGHT = 420;
+    public static final int BUTTON_COUNT = 20;
+    public static final int BUTTON_FONT_SIZE = 30;
+
+}
